4e0fd513f924169ced5ad1a4b79dad9425da835d
Jose Cortes <jose_cort@live.com>
2019-04-01 20:47:56 -0600

    todos feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoterComponent } from './voter/voter.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
